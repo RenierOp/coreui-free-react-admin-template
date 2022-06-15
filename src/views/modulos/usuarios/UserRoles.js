ed7ee691038eb7cb1b85e11f25a3481b757d204a
RenierOp <renier360@gmail.com>
2022-06-15 13:29:12 -0500

    Update UserRoles.js

--- src/views/modulos/usuarios/UserRoles.js
+++ src/views/modulos/usuarios/UserRoles.js
@@ -1,15 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {
-  CCard,
-  CCardImage,
-  CCardBody,
-  CCardTitle,
-  CCardText,
-  CButton,
-  CFormInput,
-  CFormCheck,
-} from '@coreui/react'
+import { Formik, Form, Field } from 'formik'
+import { CCard, CCardBody, CCardTitle } from '@coreui/react'
 import axios from 'axios'
 
 export default function UserRoles() {
@@ -17,6 +9,10 @@ export default function UserRoles() {
   const { id } = useParams()
   const [user, setUser] = useState([])
   const [roles, setRoles] = useState([])
+
+  const [userRoles, setUserRoles] = useState([])
+  const [formularioEnviado, setFormularioEnviado] = useState('')
+  const [asyncLoad, setAsyncLoad] = useState(false)
 
   useEffect(() => {
     getUserById()
@@ -29,8 +25,21 @@ export default function UserRoles() {
       const response = await axios.get(url, {
         headers: { Authorization: `Bearer ${auth}` },
       })
-      console.log(response.data)
       setUser(response.data)
+      //console.log(response.data.roles)
+      var tmp = new Array()
+      //tmp.push('Admin')
+      response.data.roles.map(
+        (role) =>
+          //console.log("Despues:")
+          tmp.push(role.name),
+        //setUserRoles()
+      )
+
+      //console.log(tmp)
+      setUserRoles(tmp)
+      setAsyncLoad(true)
+      //console.log(response.data)
     } catch (error) {}
   }
   const getAllRoles = async () => {
@@ -39,22 +48,69 @@ export default function UserRoles() {
       const response = await axios.get(url, {
         headers: { Authorization: `Bearer ${auth}` },
       })
-      console.log(response.data)
+      //console.log(response.data)
       setRoles(response.data)
     } catch (error) {}
   }
   return (
-    <CCard>
-      <CCardBody>
-        <CCardTitle>Nombre:</CCardTitle>
-        <p className="form-control">{user.name}</p>
-        <CCardText>Listado de roles</CCardText>
-        {roles.map((role) => (
-          <CFormCheck key={role.id} id="flexCheckDefault" label={role.name} defaultChecked />
-        ))}
+    <>
+      {asyncLoad ? (
+        <CCard>
+          <CCardBody>
+            <CCardTitle>Nombre:</CCardTitle>
+            <p className="form-control">{user.name}</p>
+            <Formik
+              initialValues={{
+                roles: userRoles,
+              }}
+              validate={(valores) => {
+                let errores = {}
 
-        <CButton>Enviar</CButton>
-      </CCardBody>
-    </CCard>
+                return errores
+              }}
+              onSubmit={async (valores, { resetForm }) => {
+                try {
+                  const url = `http://127.0.0.1:8000/api/users/roles`
+                  //resetForm()
+                  //console.log(valores.roles)
+                  const response = await axios({
+                    method: 'POST',
+                    url: url,
+                    data: {
+                      id: id,
+                      roles: valores.roles,
+                    },
+                    headers: { Authorization: `Bearer ${auth}` },
+                  })
+                  setFormularioEnviado(response.data.message)
+                  //console.log(response.data)
+                } catch (error) {}
+              }}
+            >
+              {({ errors }) => (
+                <Form>
+                  <div>
+                    {roles.map((role) => (
+                      <div key={role.id}>
+                        <label>
+                          <Field type="checkbox" name="roles" value={role.name} /> {role.name}
+                        </label>
+                      </div>
+                    ))}
+                  </div>
+
+                  <button type="submit" className="btn btn-primary">
+                    Enviar
+                  </button>
+                  {formularioEnviado && <p className="alert alert-success">{formularioEnviado}</p>}
+                </Form>
+              )}
+            </Formik>
+          </CCardBody>
+        </CCard>
+      ) : (
+        <div>Cargando...</div>
+      )}
+    </>
   )
 }
